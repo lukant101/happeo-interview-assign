6f02a80ff87a55eb2974deb685b074b09cb7c1af
Lukasz Antos <lukasz@mac.local>
2023-07-06 01:45:18 -0400

    fix: prevent next batch overwriting results of previous batch
    flag the request for the batch

--- src/interceptor.js
+++ src/interceptor.js
@@ -1,64 +1,99 @@
 import apiClient from "./apiClient.js";
 
+const batchedRequestFlag = "_isBatchedRequest";
+
 function batchInterceptor(instance) {
   // Add your code here
-  instance.interceptors.request.use(batchRequests, handleBatchError);
+  instance.interceptors.request.use(
+    handleBatchRequestsWrapper,
+    handleBatchRequestsError
+  );
   instance.interceptors.response.use(handleBatchedResults);
 }
 
 const batchUrl = "/file-batch-api";
 const batchInterval = 2000;
 
-let batching = false;
-
 const batchIds = new Set();
 
-let batchResults = null;
+let currentBatchHandler = null;
 
-function batchRequests(config) {
-  if (!batching && config.url === batchUrl) {
-    if (!batchIds.size) {
-      batchResults = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          batching = true;
-          const ids = [...batchIds];
-          batchIds.clear();
-          apiClient.get(batchUrl, { params: { ids } }).then((response) => {
-            resolve(response);
-            batching = false;
-          });
-        }, batchInterval);
-      });
-    }
-    config.params.ids.forEach((id) => {
-      batchIds.add(id);
-    });
-    config.adapter = function (config) {
-      return new Promise((resolve, reject) => {
-        const response = {
-          data: { items: null },
-        };
+function handleBatchRequestsFactory() {
+  let batchResults = null;
 
-        return batchResults.then((results) => {
-          response.data.items = results.filter((el) =>
-            config.params.ids.includes(el.id)
-          );
-          return resolve(response);
+  function handleBatchRequests(config) {
+    if (!config[batchedRequestFlag] && config.url === batchUrl) {
+      if (!batchIds.size) {
+        batchResults = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            const ids = [...batchIds];
+            batchIds.clear();
+            apiClient
+              .get(batchUrl, { params: { ids }, [batchedRequestFlag]: true })
+              .then((response) => {
+                resolve(response);
+              })
+              .catch((err) => reject(err));
+          }, batchInterval);
         });
+      }
+      const requestIds = new Set(config.params.ids);
+      config.params.ids.forEach((id) => {
+        batchIds.add(id);
       });
-    };
+      config.adapter = function (config) {
+        return new Promise((resolve, reject) => {
+          const response = {
+            data: { items: null },
+          };
+
+          return batchResults.then((results) => {
+            const fetchedFiles = results.filter((el) => requestIds.has(el.id));
+
+            if (fetchedFiles.length !== requestIds.size) {
+              const unfetchedIds = getUnfetfchedIds(requestIds, fetchedFiles);
+
+              return reject(
+                `Error. Could not fetch files: ${unfetchedIds.join(", ")}`
+              );
+            }
+            response.data.items = fetchedFiles;
+            return resolve(response);
+          });
+        });
+      };
+    }
+    return config;
   }
-  return config;
+
+  return handleBatchRequests;
 }
 
-function handleBatchError(error) {
-  console.log("error", err);
+function handleBatchRequestsWrapper(config) {
+  if (!batchIds.size || !currentBatchHandler) {
+    currentBatchHandler = handleBatchRequestsFactory();
+  }
+  return currentBatchHandler(config);
+}
+
+function handleBatchRequestsError(error) {
+  console.log("error", error);
   return Promise.reject(error);
 }
 
 function handleBatchedResults(response) {
-  // console.log("response in response interceptor", response);
   return response.data.items;
+}
+
+function getUnfetfchedIds(requestIds, fetchedFiles) {
+  const unfetchedIds = [];
+  const fetchedIds = new Set(fetchedFiles.map((el) => el.id));
+  requestIds.forEach((id) => {
+    if (!fetchedIds.has(id)) {
+      unfetchedIds.push(id);
+    }
+  });
+  return unfetchedIds;
 }
 
 export default batchInterceptor;
